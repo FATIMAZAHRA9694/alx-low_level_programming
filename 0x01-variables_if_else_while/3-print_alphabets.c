d374c1c88f5b85632e59c5bc5e3a10ac7c143c4a
fatima zahra bouhia <fatima.zahra.bouhia96@gmail.com>
2023-06-16 08:03:17 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 65;
+
+	while (a <= 90)
+{
+	putchar (a);
+	a++;
+}
+int b = 97;
+
+while (b <= 122)
+{
+	putchar (b);
+	b++;
+}
+putchar ('\n');
+return (0);
+}
