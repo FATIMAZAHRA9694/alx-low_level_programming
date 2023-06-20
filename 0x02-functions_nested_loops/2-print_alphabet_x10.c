154eb32216883811bd2bc05b2e45598027035127
fatima zahra bouhia <fatima.zahra.bouhia96@gmail.com>
2023-06-20 07:00:23 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* main - print_alphabet_x10
+* print_alphabet_x10- to print alphabet 10 times
 *
 *return - void
 */
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 
 		while (n < 10)
 		{
-			for (i=97 ; i <= 122; i++)
+			for (i = 97 ; i <= 122; i++)
 			{_putchar(i);
 			}
 		_putchar('\n');
